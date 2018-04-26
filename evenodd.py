23280db3893a8dd4165aefa5d19ac63754816e24
shah <c-shah.faisal@timesinternet.in>
2018-04-26 15:16:55 +0530

    odd&even

---
+++ evenodd.py
@@ -0,0 +1,6 @@
+x=input('enter your number that you want to check even or odd')
+newx=int(x)
+if newx%2 == 0:
+    print('The entered number {0} is even!'.format(newx))
+else:
+    print('The entered number {0} is odd!'.format(newx))
