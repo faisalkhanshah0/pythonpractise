97f3b2298e5b5fd3fbb0cfbab82e6791b54d54e9
shah <c-shah.faisal@timesinternet.in>
2018-04-26 15:37:57 +0530

    table of any no. with else

--- tableofanynumberusingfor.py
+++ tableofanynumberusingfor.py
@@ -1,4 +1,6 @@
 x=int(input('enter any no. to know the table of it.'))
-
+print('-----------------table starts from here-----------------')
 for i in range(1,11):
     print('{0} X {1} = {2}'.format(x,i,x*i))
+else:
+    print('-----------------table ended here-----------------')
