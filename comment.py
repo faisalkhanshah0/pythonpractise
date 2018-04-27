3d6b0b79060aef14f7dc9827089e897d755f47ad
shah <c-shah.faisal@timesinternet.in>
2018-04-27 15:57:44 +0530

    comments  type & global and local vars

--- comment.py
+++ comment.py
@@ -5,3 +5,16 @@ asdfasdf
 """
 
 print('hello comment')
+global x;
+def abc():
+    print('inside function abc')
+    x=4
+    print(x)
+def xyz():
+    global x;
+    x=5
+    print('inside function xyz')
+    print(x)
+xyz()
+print(x)
+abc()
