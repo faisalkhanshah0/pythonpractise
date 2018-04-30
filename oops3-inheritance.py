1a274565d6dc0ef13b80478ea1919d0dadde6f74
shah <c-shah.faisal@timesinternet.in>
2018-04-30 16:17:44 +0530

    simple inherit very important

---
+++ oops3-inheritance.py
@@ -0,0 +1,41 @@
+class One:
+    """One class"""
+    def __init__(self, x, y):
+        print('init of one');
+    def func1(self):
+        print('func1 of class one.');
+
+class Two(One):
+    """Two class"""
+    def __init__(self, x, y, z):
+        print('init of two');
+    def func2(self):
+        print('func2 of class two');
+        One.func1(self);
+
+class Three(One):
+    """Three class"""
+    def __init__(self, x, y, z):
+        print('init of three');
+    def func3(self):
+        print('func3 of class three');
+        One.func1(self);
+
+one = One(1,2);
+one.func1();
+print('-----------');
+
+two = Two(20,30,40);
+two.func1(); #making calls from parent class
+two.func2();
+print('-----------');
+
+three = Three(98,99,100);
+three.func1(); #making calls from parent class
+three.func3();
+print('------------');
+
+objects = [two, three];
+for obj in objects:
+    obj.func1();
+    print('*************');
