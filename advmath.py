722cad4d14fcc5de8a6bac75f014ee3257bbe249
shah <c-shah.faisal@timesinternet.in>
2018-04-30 13:35:07 +0530

    advance of string and math + list + tuple + dict + set

---
+++ advmath.py
@@ -0,0 +1,8 @@
+#some eg of mathematical functions
+import math;
+x=int(input('enter any no. to calculate factorial: '));
+fact = math.factorial(x);
+
+print('factorial of given no. : ',fact);
+
+# print('pi value: ', math.pi)
--- advstring.py
+++ advstring.py
@@ -25,3 +25,43 @@ x=[2,1,6,3,6,4,7]
 print(min(x)) #mathematical functions of min and max finding
 print(max(x))
 print(text.count('e'))  #finding position of given char in string
+
+#advance operation on list
+lists = ['apple', 'mango', 'banana', 'grapes', 'guava', 'orange'];  #mutablity ex
+
+print(lists[1]);
+newlist = lists[1:3];
+print(newlist);
+newlist.append('kiwi');
+print(newlist);
+# del newlist[2]
+# same as
+newlist[2:] = [];
+print(newlist)
+# print(lists.reverse()) #not working right now
+newlist.clear();
+print(newlist)
+
+tuples = ('apple', 'mango', 'banana', 'grapes')
+print(tuples[2:4]);
+# tuples[0] = 'not possible';  # immutablality
+
+# dict
+dicts = {1:'shah',2:'faisal',3:'khan'};
+print(dicts[2]);
+dicts2 = {'fname':'shah','sname':'faisal','lname':'khan'};
+print(dicts2['lname']);
+dicts2['class'] = 'A' #APPENDING DICTIONARY WITH KEY VALUE PAIR
+dicts2['extra'] = 'extra'
+del dicts2['extra']  #deleting key value pair
+print(dicts2);
+
+
+#set eg
+
+bri = set(['brazil', 'russia', 'india'])
+bri.add('china');
+bric = bri.copy();
+print('india' in bri);
+print(bri)
+print(bric)
