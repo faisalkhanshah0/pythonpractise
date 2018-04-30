92edc54ccc7db763b2e66785692d6e5e3d54b14c
shah <c-shah.faisal@timesinternet.in>
2018-04-30 11:40:38 +0530

    Import export examples

---
+++ manualimportin.py
@@ -0,0 +1,26 @@
+#custom modules example
+
+import manualimportout;
+
+x = int(input('enter first no.'));
+y = int(input('enter second no.'));
+
+result = manualimportout.addi(x,y);
+print('sum of given no. is : ',result);
+print(manualimportout._version_);
+
+
+#inbuilt modules example
+#import sys;
+#print('the path is : ',sys.path);
+
+#importing only specific object from modules
+#from sys import path;
+#print('the path is :',path);
+#modifying new value to old
+#path='xyz'
+#print('New value: ',path);
+
+#importing all objects from modules
+from sys import *;
+print('the path is: ',path);
---
+++ manualimportout.py
@@ -0,0 +1,16 @@
+def addi(x,y):
+    return x+y;
+
+def subt(x,y):
+    return x-y;
+
+def mult(x,y):
+    return x*y;
+
+def divi(x,y):
+    return x/y;
+
+def modu(x,y):
+    return x%y;
+
+_version_ = '1.0'
