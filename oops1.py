3b0f990c8374455ff8ff53709db7f6f5f70b413b
shah <c-shah.faisal@timesinternet.in>
2018-04-30 15:55:13 +0530

    oops concept 1 very important

--- oops1.py
+++ oops1.py
@@ -11,6 +11,22 @@ class Employee(object):
         print("Name : {0}, Salary : {1}".format(self.name, self.salary));
 
 emp1 = Employee('Mohan',10000);
+
+emp1.email="c-shah.faisal@timesinternet.in" # adding prop at runtime
+print(emp1.email);
+del emp1.email; #deleting attr of object at runtime
+# del keyword also remove the reference of the given attr so that garbase collection esily detect which prop to delete.
+
+if hasattr(emp1,'email'):
+    print('Found email : ');
+    print(getattr(emp1,'email'));
+else:
+    print('Email not found & we have created it');
+    setattr(emp1,'email','c-shah.faisal@timesinternet.in');
+    print(getattr(emp1,'email'));
+    del emp1.email; #deleting attr of object at runtime
+
+
 emp1.displayCount();
 emp1.displayEmployee();
 emp2 = Employee('Atul',15000);
@@ -19,3 +35,15 @@ emp2.displayEmployee();
 emp3 = Employee('Vivek',12000);
 emp3.displayCount();
 emp3.displayEmployee();
+
+""" shallow copy both objects referencing to same location that
+means if you change value of prop of any object leads to change in another."""
+emp4 = emp3;
+emp4.displayCount();
+emp4.displayEmployee();
+
+"""deep copy of object and its inner content like object.create in javascript"""
+import copy;
+emp5 = copy.copy(emp2);
+emp5.displayCount();
+emp5.displayEmployee();
---
+++ oops2.py
@@ -0,0 +1,13 @@
+class Employee:
+    pass
+
+class Manager(Employee):
+    'This is documentation'
+    def personalDetails():
+        pass;
+
+print("name :",Manager.__name__); # Class name
+print("Bases :",Manager.__bases__); # A possily empty tuple containing the base classes, in the order of their occurence in the base class list.
+print("Doc :",Manager.__doc__); # Class documentation string, or None if undefined.
+print("Module :",Manager.__module__); # Module name in which the class is defined. This attr is "__main__" in interactive mode
+print("Dict :",Manager.__dict__);  # Contains the class's namespace
