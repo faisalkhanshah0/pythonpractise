74001f13d01d129956f3d2863bfd711a320f947e
shah <c-shah.faisal@timesinternet.in>
2018-05-01 13:51:07 +0530

    file handling read/write binary

--- filehandling2write.py
+++ filehandling2write.py
@@ -1,6 +1,11 @@
-f=open('testwrite.txt','w+');
+"""f=open('testwrite.txt','w+');
 str='hello infogain \n';
 f.write(str);
 str='Best of luck!'
 f.write(str);
+f.close();"""
+
+f=open('testwrite.txt','w+');
+lines=['one\n','two\n','three\n'];
+f.writelines(lines);
 f.close();
---
+++ filehandling3writebinary.py
@@ -0,0 +1,11 @@
+import pickle;
+f=open('test.dat','wb');
+md={'a':1,'b':2,'c':3};
+pickle.dump(md, f);  #write binary in opend file stream
+f.close;
+
+
+ifl=open('test.dat','rb');
+md1=pickle.load(ifl); #reading data from binary file
+ifl.close();
+print(md1);
