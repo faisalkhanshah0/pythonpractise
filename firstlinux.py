6b345c875fde85e9c3e8de910d7e68f0ffa9a1c3
faishal <faishal@faishal.igglobal.com>
2019-04-29 16:20:05 +0530

    good example of linux scripting

--- firstlinux.py
+++ firstlinux.py
@@ -1,16 +1,41 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 
 import os;
 #import subprocess;
-from subprocess import call;
+from subprocess import call, Popen, PIPE, check_output, getoutput;
 #call(["cat", "oops1.py"]);
 #print(dir(subprocess));
 
 #subprocess.call(["mkdir", "testone"]);
 #os.system("mkdir test");
 #os.system("sudo systemctl restart jenkins")
-a = os.system("ls");
-print(123)
-print(a);
+#a = os.system("ls");
+#b= call('ls -lt', shell=True);
+#print(123)
+#print(b);
 #os.mkdir("test1");
+
+
+#To pipe output from cmd to py var
+#output3 = getoutput("ls -lt").split('\n');
+#output = check_output("ls", shell=True)
+#print(output3[0]);
+#c=Popen("ls", shell=True, stdout=PIPE, stderr=PIPE)
+
+#o, e = c.communicate();
+
+#cmd = ["echo", "hi", ">", "a.txt"]
+#call(cmd)
+
+#call("echo {0} > a.txt".format(output3), shell=True);
+#print(o);
+
+#print('Output:' + o.decode('ascii'));
+#print('Error:' + e.decode('ascii'));
+#print('Code:' + str(c.returncode));
+
+
+file_ = open("output.txt", "w");
+
+Popen("ls", shell=True, stdout=file_)
