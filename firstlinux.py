810d76d55e8e9938e73ba537dce4b7191ab823bc
faishal <faishal@faishal.igglobal.com>
2019-04-29 15:13:55 +0530

    linux file updated

--- firstlinux.py
+++ firstlinux.py
@@ -2,10 +2,15 @@
 
 
 import os;
+#import subprocess;
+from subprocess import call;
+#call(["cat", "oops1.py"]);
+#print(dir(subprocess));
 
-print(dir(os));
-
-
+#subprocess.call(["mkdir", "testone"]);
 #os.system("mkdir test");
-
+#os.system("sudo systemctl restart jenkins")
+a = os.system("ls");
+print(123)
+print(a);
 #os.mkdir("test1");
